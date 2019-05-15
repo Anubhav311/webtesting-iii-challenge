fbf073014856198f7bd22c082cafe71cb313cca0
Anubhav311 <anubhavbhambri1@gmail.com>
2019-05-16 04:21:14 +0530

    Created snapshot tests for display and dashboard.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Display from './Display';
+
+describe('<Display/>', () => {
+    it('test for display', () => {
+        const tree = renderer.create(<Display/>);
+
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
+})
